2511b42351ce9c3f002ef36e31dcfc1589038f6a
ashnal <ashnal52@gmail.com>
2018-12-27 00:55:13 +0100

    Rewrite of most of RimworldDiscoverTechsClass.cs. New ModExtension for ResearchProjectDef, and edits to ResearchProjects_Blueprints.xml to use the extension.
    No longer creates dynamic ResearchProjectDef prerequisites.
    
    Instead, uses Harmony to patch ResearchProjectDef.PrerequisitesComplete to check for the ModExtension, then performs a custom prereq check. Modifies result accordingly. If the property is being accessed for a research project that isn't one of ours, it runs the original code for best compatibility.

--- Sources/RimworldDiscoverTechs/Classes/RimworldDiscoverTechsClass.cs
+++ Sources/RimworldDiscoverTechs/Classes/RimworldDiscoverTechsClass.cs
@@ -1,60 +1,132 @@
 ﻿using Verse;
-using System;
+using Harmony;
 using RimWorld;
+using System.Reflection;
+using System.Linq;
+using System;
 using System.Collections.Generic;
 
 namespace RimworldDiscoverTechs
 {
     [StaticConstructorOnStartup]
-    class RimworldDiscoverTechsClass
+    public static class RimworldDiscoverTechsClass
     {
         static RimworldDiscoverTechsClass()
         {
-            Log.Message("### Technology Blueprints mod loaded. ###");
+            //Create our Harmony instance to run the patch
+            HarmonyInstance.Create("RimworldDiscoverTechsClass").PatchAll();
 
-            // Go through research projects, create lists according to techLevels, then add those projectDefs as prerequisites to each technology blueprint researchDef accordingly.
-            // First we create lists
-            List<ResearchProjectDef> listOfBPs = new List<ResearchProjectDef>(4);
-            List<List<ResearchProjectDef>> listOfResearchLists = new List<List<ResearchProjectDef>>(4);
+            Log.Message("Technology Blueprints :: Loading finished, We hope you enjoy Technology Blueprints!");
+        }
+    }
 
-            List<ResearchProjectDef> researchProjectsNeolithic = DefDatabase<ResearchProjectDef>.AllDefsListForReading.FindAll((ResearchProjectDef x) => x.techLevel == TechLevel.Neolithic);
-            listOfResearchLists.Add(researchProjectsNeolithic);
-            ResearchProjectDef NeolithicBP = researchProjectsNeolithic.Find((ResearchProjectDef x) => x.defName == "NeolithicBlueprint");
-            researchProjectsNeolithic.Remove(NeolithicBP);
-            listOfBPs.Add(NeolithicBP);
+    //DefModExtension so the prefix code can distinguish between our special ResearchProjecDefs and normal ones
+    // to allow for different treatment
+    public class CompleteTechLevelPrereq : DefModExtension
+    {
+        //Value is the entire TechLevel that is the prerequisite
+        public TechLevel techLevel;
+    }
 
-            List<ResearchProjectDef> researchProjectsMedieval = DefDatabase<ResearchProjectDef>.AllDefsListForReading.FindAll((ResearchProjectDef x) => x.techLevel == TechLevel.Medieval);
-            listOfResearchLists.Add(researchProjectsMedieval);
-            ResearchProjectDef MedievalBP = researchProjectsMedieval.Find((ResearchProjectDef x) => x.defName == "MedievalBlueprint");
-            researchProjectsMedieval.Remove(MedievalBP);
-            listOfBPs.Add(MedievalBP);
-
-            List<ResearchProjectDef> researchProjectsIndustrial = DefDatabase<ResearchProjectDef>.AllDefsListForReading.FindAll((ResearchProjectDef x) => x.techLevel == TechLevel.Industrial);
-            listOfResearchLists.Add(researchProjectsIndustrial);
-            ResearchProjectDef IndustrialBP = researchProjectsIndustrial.Find((ResearchProjectDef x) => x.defName == "IndustrialBlueprint");
-            researchProjectsIndustrial.Remove(IndustrialBP);
-            listOfBPs.Add(IndustrialBP);
-
-            List<ResearchProjectDef> researchProjectsSpacer = DefDatabase<ResearchProjectDef>.AllDefsListForReading.FindAll((ResearchProjectDef x) => x.techLevel == TechLevel.Spacer);
-            listOfResearchLists.Add(researchProjectsSpacer);
-            ResearchProjectDef SpacerBP = researchProjectsSpacer.Find((ResearchProjectDef x) => x.defName == "SpacerBlueprint");
-            researchProjectsSpacer.Remove(SpacerBP);
-            listOfBPs.Add(SpacerBP);
-
-            // Then we update the prereqs
-            for (int i = 0; i < listOfBPs.Count; i++)
+    //Here is the patch for ResearchProjectDef.PrerequisitesComplete
+    //Runs before the original method as a Prefix
+    //Checks the Def for our Mod Extension
+    //Our Defs run a custom check for completion of all tech of the TechLevel specified in the Def
+    //Sets the result to true or false depending on our check, and ignores original method.
+    //If the Def being checked isn't one of ours, we skip our code and continue to the original method.
+    [HarmonyPatch(typeof(ResearchProjectDef))]
+    [HarmonyPatch("PrerequisitesCompleted", MethodType.Getter)]
+    public static class PrerequisitesCompleted_Prefix
+    {
+        [HarmonyPrefix]
+        public static bool Prefix(ref bool __result, ResearchProjectDef __instance)
+        {
+            if (__instance.HasModExtension<CompleteTechLevelPrereq>())
             {
-                Log.Message("Checking " + listOfBPs[i].defName);
+                //Log.Message("Technology Blueprints :: PrerequisitesCompleted HasModExtension " + __instance);
 
-                foreach (ResearchProjectDef researchProject in listOfResearchLists[i])
+                List<ResearchProjectDef> unfinishedProjects = DefDatabase<ResearchProjectDef>
+                    .AllDefsListForReading
+                    .FindAll((ResearchProjectDef x) => x.techLevel == __instance.GetModExtension<CompleteTechLevelPrereq>().techLevel
+                        && !x.HasModExtension<CompleteTechLevelPrereq>()
+                        && !x.IsFinished);
+
+                
+
+                if (unfinishedProjects.NullOrEmpty())
                 {
-                    listOfBPs[i].prerequisites.Add(researchProject);
-                    Log.Message("+ Prerequisite " + researchProject.defName);
+                    __result = true;
+                    return false;
+                }
+                else
+                {
+                    //Log.Message("Technology Blueprints :: unfinished techs = " + unfinishedProjects.Join(null, ", "));
+                    __result = false;
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    [HarmonyPatch]
+    public static class ResearchNode_Get_Available
+    {
+        public static MethodBase target;
+
+        public static bool Prepare()
+        {
+            var mod = LoadedModManager.RunningMods.FirstOrDefault(m => m.Name == "ResearchPal" || m.Name == "Research Tree");
+            if (mod == null)
+            {
+                return false;
+            }
+            Log.Message("Technology Blueprints :: " + mod.Name + " detected, patching ...");
+
+            Type type = null;
+
+            foreach (Assembly a in mod.assemblies.loadedAssemblies)
+            {
+                Type typeResult = a.GetTypes().ToList().Find((Type t) => t.Name == "ResearchNode");
+                if (typeResult != null)
+                {
+                    type = typeResult;
                 }
             }
 
-            Log.Message("Technology Blueprints: modified prerequisites for Technology Blueprints research projects.");
-            Log.Message("### We hope you enjoy Technology Blueprints!###");
+            if (type == null)
+            {
+                Log.Warning("Technology Blueprints :: Can't patch " + mod.Name + " No ResearchNode type");
+                return false;
+            }
+
+            target = AccessTools.Property(type, "Available").GetGetMethod();
+            if (target == null)
+            {
+                Log.Warning("Technology Blueprints :: Can't patch " + mod.Name + " No ResearchNode.Available Property");
+                return false;
+            }
+
+            return true;
+        }
+
+        public static MethodBase TargetMethod()
+        {
+            return target;
+        }
+
+        public static bool Prefix(ref bool __result, object __instance)
+        {
+            ResearchProjectDef research = AccessTools.Field(__instance.GetType(), "Research").GetValue(__instance) as ResearchProjectDef;
+
+            bool hasExtension = research.HasModExtension<CompleteTechLevelPrereq>();
+            if (hasExtension)
+            {
+                //Log.Message("Technology Blueprints Rsearch Pal/Tree patch :: " + research + ", divert to PrerequisitesCompleted patch");
+                __result = research.PrerequisitesCompleted;
+                return false;
+            }
+            return true;
         }
     }
 }
